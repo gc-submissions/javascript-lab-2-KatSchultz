5af3aa32efa3f32ca130fcb62ced3314e1ca1ee0
Katie Schultz <ks.katieschultz@gmail.com>
2022-05-01 21:47:27 -0400

    created randomDamage and chooseOption formulas

--- src/script.js
+++ src/script.js
@@ -1,3 +1,14 @@
 "use strict";
 
 // TODO - write your code here.
+
+const randomDamage = () => Math.trunc(Math.random() * 10 + 1);
+
+console.log("random damage:", randomDamage());
+
+const chooseOption = (opt1, opt2) => {
+  let randNum = Math.round(Math.random());
+  return randNum === 0 ? opt1 : opt2;
+};
+
+console.log(chooseOption("red", "blue"));
